5061bede42c7b317fdcaeefaad3abdfc9c720cdb
HieuLM-AVISPL <hm15015@avispl.com>
2023-09-08 15:24:36 +0700

    SYAL-draft-code draft code of MagicInfo adapter

---
+++ src/main/java/com/avispl/symphony/dal/infrastructure/management/samsung/magicinfo/MagicInfoCommunicator.java
@@ -0,0 +1,11 @@
+package com.avispl.symphony.dal.infrastructure.management.samsung.magicinfo;
+
+/**
+ * MagicInfoCommunicator
+ *
+ * @author Kevin / Symphony Dev Team<br>
+ * Created on 9/8/2023
+ * @since 1.0.0
+ */
+public class MagicInfoCommunicator {
+}
